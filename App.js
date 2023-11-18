9ace4078aa7223c327e2d607552cbcfdc7bb7908
Say <reubenlouis64@gmail.com>
2023-11-18 21:47:36 +0800

    Add global styling

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
+import {faEnvelope} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, Text, TouchableOpacity, View} from 'react-native';
+import {globalStyle} from './assets/styles/globalStyle';
 import {Title} from './components/title/title';
 
 const App = () => {
   return (
     <SafeAreaView>
-      <Title title={'Let’s Explore '} />
+      <View style={globalStyle.header}>
+        <Title title={'Let’s Explore '} />
+        <TouchableOpacity style={globalStyle.messageIcon}>
+          <FontAwesomeIcon icon={faEnvelope} size={20} color={'#898DAE'} />
+          <View style={globalStyle.messageNumberContainer}>
+            <Text style={globalStyle.messageNumber}>2</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
